e278823bd6f62fb2d8afee4bd2bbe3a539bdfa13
Minyus <yusuke.minami86@gmail.com>
2019-10-25 15:07:39 +0800

    Add numpy channel order conversion

--- src/kedex/contrib/ops/pytorch_ops.py
+++ src/kedex/contrib/ops/pytorch_ops.py
@@ -380,3 +380,23 @@ class PytorchUnsqueeze(torch.nn.Module):
 
     def forward(self, input):
         return torch.unsqueeze(input, dim=self.dim)
+
+
+_to_channel_last_dict = {3: (-2, -1, -3), 4: (0, -2, -1, -3)}
+
+
+def to_channel_last_arr(a):
+    if a.ndim in {3, 4}:
+        return np.transpose(a, axes=_to_channel_last_dict.get(a.ndim))
+    else:
+        return a
+
+
+_to_channel_first_dict = {3: (-1, -3, -2), 4: (0, -1, -3, -2)}
+
+
+def to_channel_first_arr(a):
+    if a.ndim in {3, 4}:
+        return np.transpose(a, axes=_to_channel_first_dict.get(a.ndim))
+    else:
+        return a
