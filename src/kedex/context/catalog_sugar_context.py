0cbdd80855d7fc7ca7375bebf1e6d727c6b1e9b2
Minyus <yusuke.minami86@gmail.com>
2019-10-28 14:52:53 +0800

    Allow overriding

--- src/kedex/context/catalog_sugar_context.py
+++ src/kedex/context/catalog_sugar_context.py
@@ -44,32 +44,32 @@ class CatalogSyntacticSugarContext(KedroContext):
             ds_dict = copy.deepcopy(default_dict)
             if isinstance(ds_dict_, dict):
                 ds_dict.update(ds_dict_)
-            ds_name, ds_dict = _format_kedro_dataset(ds_name, ds_dict)
+            _check_type(ds_dict)
+            ds_name, ds_dict = self._format_kedro_dataset(ds_name, ds_dict)
             conf_catalog_processed[ds_name] = ds_dict
         return conf_catalog_processed
 
+    def _format_kedro_dataset(self, ds_name, ds_dict):
+        ds_name, ds_dict = self._set_filepath(ds_name, ds_dict)
+        ds_name, ds_dict = self._enable_caching(ds_name, ds_dict)
+        return ds_name, ds_dict
 
-def _format_kedro_dataset(ds_name, ds_dict):
-    _check_type(ds_dict)
-    ds_name, ds_dict = _set_filepath(ds_name, ds_dict)
-    ds_name, ds_dict = _enable_caching(ds_name, ds_dict)
-    return ds_name, ds_dict
+    def _set_filepath(self, ds_name, ds_dict):
+        if "filepath" not in ds_dict:
+            ds_dict["filepath"] = ds_name
+            ds_name = Path(ds_name).stem
+        return ds_name, ds_dict
 
-
-def _set_filepath(ds_name, ds_dict):
-    if "filepath" not in ds_dict:
-        ds_dict["filepath"] = ds_name
-        ds_name = Path(ds_name).stem
-    return ds_name, ds_dict
-
-
-def _enable_caching(ds_name, ds_dict):
-    cached = False
-    if "cached" in ds_dict:
-        cached = ds_dict.pop("cached")
-    if cached and (ds_dict.get("type") != "kedro.contrib.io.cached.CachedDataSet"):
-        ds_dict = {"type": "kedro.contrib.io.cached.CachedDataSet", "dataset": ds_dict}
-    return ds_name, ds_dict
+    def _enable_caching(self, ds_name, ds_dict):
+        cached = False
+        if "cached" in ds_dict:
+            cached = ds_dict.pop("cached")
+        if cached and (ds_dict.get("type") != "kedro.contrib.io.cached.CachedDataSet"):
+            ds_dict = {
+                "type": "kedro.contrib.io.cached.CachedDataSet",
+                "dataset": ds_dict,
+            }
+        return ds_name, ds_dict
 
 
 def _check_type(ds_dict):
