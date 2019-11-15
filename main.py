c9e3ac6bc2ca7fe5f6574da185de3888bd7c1944
Minyus <yusuke.minami86@gmail.com>
2019-11-15 20:04:04 +0800

    Update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if str(src_path) not in sys.path:
 if "PYTHONPATH" not in os.environ:
     os.environ["PYTHONPATH"] = src_path
 
-from kedex.mlflow_context import MLflowFlexibleContext
+from kedex import MLflowFlexibleContext
 
 
 context = MLflowFlexibleContext(project_path)
